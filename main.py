54eae7787e0764ac4ebe2ad61e32cf53b42c8e8e
meekh <meekhaylo1@gmail.com>
2022-10-03 18:23:47 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from secondary.addition import *
+from secondary.mymod import *
+import sys
+
+print("\nHere goes task 1")
+task1 = [1, 2, 3, 4, 5]
+multiply_func(*task1)
+
+
+print("\nHere goes task 2")
+print(sys.path)
+sys.path.append("C:/Users/meekh/python")
+print("\n", sys.path)
+
+
+print("\nHere goes task 3")
+count_lines("C:/Users/meekh/python/records.txt")
+count_chars("C:/Users/meekh/python/records.txt")
+test("C:/Users/meekh/python/records.txt")
